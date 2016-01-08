0bd48776bdea0c24a6ff68d53b8bab89139fd81d
Spencer Smith <ssmith@egi.utah.edu>
2016-01-08 09:45:05 -0700

    Initial commit. Added Image Pull ver. 1

---
+++ src/com/spencerlarry/Pull.java
@@ -0,0 +1,14 @@
+package com.spencerlarry;
+
+import javax.swing.JPanel;
+
+public class Pull {
+
+	public static void main(String[] args) {
+		
+		View view = new View();
+		view.setVisible(true);
+		
+	}
+
+}
